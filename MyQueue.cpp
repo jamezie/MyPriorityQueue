139a2473a1cdcce21725a75a3ebdf58f33bcb18e
James K <jame_zie@yahoo.com>
2016-06-28 19:31:18 -0700

    bubbleUp written, not tested

--- MyQueue.cpp
+++ MyQueue.cpp
@@ -9,12 +9,41 @@ private:
 
 	void bubbleDown(){
 
-		}
-
-	void bubbleUp(){
-
 	}
 
+	void bubbleUp(int bubblePosition){
+		if(bubblePosition < 1) return;
+		Node::Node * bubbleNode = queue[bubblePosition];
+
+		int parentPosition = bubblePosition/2;
+		if(parentPosition < 1) return;
+
+		Node::Node * parentNode = queue[parentPosition];
+
+		if(parentNode->getPriority() < bubbleNode->getPriority()) {
+			swapNodes(bubblePosition, parentPosition);
+			bubbleUp(parentPosition);
+		}
+	}
+
+	Node::Node * getLast() {
+		return queue[tail];
+	}
+
+	Node::Node * getLargerChild() {
+		return 0;
+	}
+
+	void swapNodes(int position1, int position2) {
+		Node::Node * node1 = queue[position1];
+		Node::Node * node2 = queue[position2];
+
+		if(!(node1 && node2))
+			return;
+
+		queue[position2] = node1;
+		queue[position1] = node2;
+	}
 
 public:
 	MyQueue(int size): tail(INIT_POSITION) {
@@ -31,15 +60,13 @@ public:
 	}
 
 	Node::Node * pop(){
-		return queue[INIT_POSITION];
-	}
+		Node::Node * poppedNode = queue[INIT_POSITION];
 
-	Node::Node * getLast() {
-		return queue[tail];
+		return poppedNode;
 	}
 
 	Node::Node * top(){
-		return queue[1];
+		return queue[INIT_POSITION];
 	}
 };
 
